cf689cd2d1468ad062bf095428d0a645e1118c0e
jynti <33417806+jynti@users.noreply.github.com>
2017-11-08 15:18:58 +0530

    added default message to exception classes

--- Csv/lib/exception.rb
+++ Csv/lib/exception.rb
@@ -1,5 +1,7 @@
 class InvalidExtensionError < StandardError
-
+  def initialize
+    super("Only csv files valid")
+  end
 end
 
 class ClassAlreadyExistsError < StandardError
@@ -7,10 +9,13 @@ class ClassAlreadyExistsError < StandardError
 end
 
 class SpaceInsideFileNameError < StandardError
-
+  def initialize
+    super("There is a whitespace in your file name")
+  end
 end
 
 class InvalidHeaderError < StandardError
 
 end
+
 
